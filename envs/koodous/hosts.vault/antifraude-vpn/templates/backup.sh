2105cd5bf6dbb15cd09d0e63846e1f3018a4f742
zisk0 <flopez@hispasec.com>
2016-05-10 02:32:42 +0200

    Checked for sharing

--- envs/koodous/hosts.vault/antifraude-vpn/templates/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BKFOLDER="/home/backuo/backup"
-
-# Ficheros de configuracion del servidor
-mkdir -p $BKFOLDER
-rsync -av /etc $BKFOLDER/
-
-chown backuo.backuo $BKFOLDER -Rf
--- playbooks/defaultPass.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-for user in $@
-    do
-        echo -e "12345678\n12345678" | passwd $user
-    done
